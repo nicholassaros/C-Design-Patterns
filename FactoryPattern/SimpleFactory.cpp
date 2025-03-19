2abedc458e03497ae46c59ce0b668150ac1a76d7
Nicholas Saros <nicholas.saros.saros@oracle.com>
2025-03-18 22:32:30 -0500

    added in Naive Logger Factory

--- FactoryPattern/SimpleFactory.cpp
+++ FactoryPattern/SimpleFactory.cpp
@@ -3,27 +3,57 @@
 
 using namespace std;
 
-class ILoggerFactor {
-    virtual void Log() = 0;
-    virtual ~ILoggerFactor();
+class ILogger {
+    public:
+        virtual void Log(string message) = 0;
+        virtual ~ILogger() {}
 };
 
-class FileLogger : public ILoggerFactor {
-
-};
-
-class ConsoleLogger : public ILoggerFactor {
-
-};
-
-class DatabaseLogger : public ILoggerFactor {
+class FileLogger : public ILogger {
+    public:
+        FileLogger() {}
+        void Log(string message) override {}
 
 };
 
-class JsonLogger : public ILoggerFactor {
-
+class ConsoleLogger : public ILogger {
+    public:
+        ConsoleLogger() {}
+        void Log(string message) override {}
 };
 
+class DatabaseLogger : public ILogger {
+    public:
+        DatabaseLogger() {}
+        void Log(string message) override{}
+};
+
+class JsonLogger : public ILogger {
+    public: 
+        JsonLogger() {}
+        void Log(string message) override{}
+};
+
+
+/*
+    In this naive implementation we use a concrete factory class which has a method that
+    take in a string type and creates the corresponding object. This can be improved as
+    this will eventually become un-maintainable as we add more loggers
+*/
+class ConcreteLoggerFactory {
+    public:
+        ILogger* createLogger(string type){
+            if(type == "FileLogger"){
+                return new FileLogger();
+            } else if (type == "ConsoleLogger"){
+                return new ConsoleLogger();
+            } else if (type == "DatabaseLogger"){
+                return new DatabaseLogger();
+            } else if (type == "JsonLogger"){
+                return new JsonLogger();
+            }
+        }
+};
 
 
 int main(){
