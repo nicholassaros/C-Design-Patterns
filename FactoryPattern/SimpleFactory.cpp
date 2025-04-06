b83a9922dbcb4fbd8dba5aced74e2bc1a2295c03
Nicholas Saros <nicholas.saros.saros@oracle.com>
2025-04-06 18:00:53 -0500

    added finished simpleFactory

--- FactoryPattern/SimpleFactory.cpp
+++ FactoryPattern/SimpleFactory.cpp
@@ -6,84 +6,82 @@
 using namespace std;
 
 class ILogger {
+    protected:
+        string m_logLevel = "INFO";
     public:
         virtual void Log(string message) = 0;
+        virtual void SetLogLevel(string level) = 0;
+        virtual string GetLogLevel() = 0;
         virtual ~ILogger() {}
 };
 
 class FileLogger : public ILogger {
     private:
-        string m_logLevel;
         string m_filePath;
-
     public:
         FileLogger()
-            : m_filePath("./log_file.log"), m_logLevel("INFO") {}
+            : m_filePath("./log_file.log") {}
 
         void Log(string message) override {
 
-            ofstream logFile(m_filePath);
+            ofstream logFile(m_filePath, ios::app);
             if(!logFile){
                 cerr <<  "Error opening file " << strerror(errno);
             }
-            logFile << m_logLevel << message << endl;
-            logFile.close();
-        }
+            string formatted_log_level = "[" + m_logLevel + "] ";
 
-        void SetLogPath(string filePath){
-            m_filePath = filePath;
+            logFile << formatted_log_level << message << endl;            
+            logFile.close();
         }
         
         void SetLogLevel(string logLevel) {
+            if (logLevel != "INFO" &&
+                logLevel != "ERROR" &&
+                logLevel != "DEBUG"){
+                
+                // invalid log level
+                cout << "Invalid Log Level" << endl;
+                exit(1);
+            }
             m_logLevel = logLevel;
         }
 
-        void SetTimeFormat(){}
+        string GetLogLevel() {
+            return m_logLevel;
+        }
 
 };
 
 class ConsoleLogger : public ILogger {
-    private:
-        string m_logLocation;
     public:
-        ConsoleLogger() 
-            : m_logLocation("OUT") {}
-
+        ConsoleLogger() {}
         void Log(string message) override {
-            if(m_logLocation == "OUT"){
-                cout << message << endl;
+            string formatted_log_level = "[" + m_logLevel + "] ";
+
+            if(m_logLevel == "ERROR"){
+                cerr << formatted_log_level << message << endl;
             } else {
-                cerr << message << endl;
+                cout << formatted_log_level << message << endl;
             }
         }
 
-        string GetLogOuput() {
-            return m_logLocation;
-        }
-
-        void SetLogOutput(string newOutput) {
-            if(newOutput != "OUT" || newOutput != "ERR"){
-                cerr << "Invalid Log Output for ConsoleLogger. Use 'OUT' or 'ERR'" << endl;
+        void SetLogLevel(string logLevel) {
+            if (logLevel != "INFO" &&
+                logLevel != "ERROR" &&
+                logLevel != "DEBUG"){
+                
+                // invalid log level
+                cout << "Invalid Log Level" << endl;
+                exit(1);
             }
-            m_logLocation = newOutput;
+            m_logLevel = logLevel;
+        }
+
+        string GetLogLevel() {
+            return m_logLevel;
         }
 };
 
-class DatabaseLogger : public ILogger {
-    public:
-        DatabaseLogger() {}
-        void Log(string message) override{}
-        void SetLogTable() {}
-        ~DatabaseLogger() {}
-};
-
-class JsonLogger : public ILogger {
-    public: 
-        JsonLogger() {}
-        void Log(string message) override{}
-        void SetFilePath() {}
-        void PrettyPrint() {}
-};
 
 
 /*
@@ -91,32 +89,101 @@ class JsonLogger : public ILogger {
     take in a string type and creates the corresponding object. This can be improved as
     this will eventually become un-maintainable as we add more loggers
 */
-
-enum LoggerType { FILE, CONSOLE, DATABASE, JSON};
+namespace Logger {
+    enum LoggerType { FILE, CONSOLE };
+}
 class ConcreteLoggerFactory {
     public:
-        ILogger* createLogger(LoggerType t){
-            if(t == LoggerType::FILE){
+        ILogger* createLogger(Logger::LoggerType t){
+            if(t == Logger::LoggerType::FILE){
                 return new FileLogger();
 
-            } else if (t == LoggerType::CONSOLE){
+            } else {
                 return new ConsoleLogger();
 
-            } else if (t == LoggerType::DATABASE){
-                return new DatabaseLogger();
-
-            } else if (t == LoggerType::JSON){
-                return new JsonLogger();  
             }
         }
 };
 
+void testFileLogger(ILogger* fileLogger){
+    fileLogger->Log("My First FileLogger Message");
+
+    fileLogger->Log("My FileLogger level: " +fileLogger->GetLogLevel());
+
+    string level = "ERROR";
+    fileLogger->SetLogLevel(level);
+
+    fileLogger->Log("Changed FileLogger level: " +fileLogger->GetLogLevel()); 
+
+}
+
+void testConsoleLogger(ILogger* consoleLogger){
+    consoleLogger->Log("My First ConsoleLogger Message");
+
+    consoleLogger->Log("My ConsoleLogger level: " +consoleLogger->GetLogLevel());
+
+    string level = "ERROR";
+    consoleLogger->SetLogLevel(level);
+
+    consoleLogger->Log("Changed ConsoleLogger level: " +consoleLogger->GetLogLevel()); 
+}
+
+
 
 int main(){
 
-    LoggerType fileLoggerType = LoggerType::FILE;
     ConcreteLoggerFactory concreteLoggerFactory;
+    int logger;
+    string msg;
 
-    ILogger* fileLogger = concreteLoggerFactory.createLogger(fileLoggerType);
+    cout << R"(
+         _                                  _____          _                   
+        | |    ___   __ _  __ _  ___ _ __  |  ___|_ _  ___| |_ ___  _ __ _   _ 
+        | |   / _ \ / _` |/ _` |/ _ \ '__| | |_ / _` |/ __| __/ _ \| '__| | | |
+        | |__| (_) | (_| | (_| |  __/ |    |  _| (_| | (__| || (_) | |  | |_| |
+        |_____\___/ \__, |\__, |\___|_|    |_|  \__,_|\___|\__\___/|_|   \__, |
+                    |___/ |___/                                          |___/ 
+    )";
+    cout << '\n';
+    cout << '\n';
+    cout << '\n';
 
+ 
+    cout << "Enter '1' for File Logger" << '\n';
+    cout << "Enter '2' for Console Logger" << '\n';
+
+    cout << "Logger Selection: " << '\n';
+    cin >> logger;
+
+    if (logger == 1) {
+        // Create fileLogger
+        Logger::LoggerType fileLoggerType = Logger::LoggerType::FILE;
+        ILogger* fileLogger = concreteLoggerFactory.createLogger(fileLoggerType);
+        cout << "Selected FileLogger " << '\n';
+
+        while(true){ 
+            getline(cin, msg);
+            fileLogger->Log(msg);
+            cout << '\n';
+        }
+
+    } else if (logger == 2) {
+        // Create consoleLogger
+        Logger::LoggerType consoleLoggerType = Logger::LoggerType::CONSOLE;
+        ILogger* consoleLogger = concreteLoggerFactory.createLogger(consoleLoggerType);
+        cout << "Selected ConsoleLogger " << '\n';
+
+        while(true){
+            getline(cin, msg);
+            consoleLogger->Log(msg);
+            cout << '\n';
+        }
+
+    } else {
+        cout << "INVALID SELECTION!";
+    }
+
+
+
+    cout << "Application Terminated";
 }
